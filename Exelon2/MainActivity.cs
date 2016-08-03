b3190afb4571482dad23389f129685856401170e
KingVizzy <vallery.salomon@gmail.com>
2016-08-03 11:30:32 -0400

    New Repository w/Updates
    New Repository with updates

---
+++ Exelon2/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Exelon2
+{
+    [Activity(Label = "Exelon2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button mBtnSignUp;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+                {
+                    //Pull up dialog box
+                    FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                    dialog_SignUp signUpDialog = new dialog_SignUp();
+                    signUpDialog.Show(transaction, "dialog fragment");
+                };
+        }
+
+       
+    }
+}
+
---
+++ Exelon2/dialog_SignIn.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Exelon2
+{
+    class dialog_SignIn : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var View = inflater.Inflate(Resource.Layout.dialog_sign_in, container, false);
+
+            return View;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle); //Sets the title bar to invisible
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation; //sets the animation
+
+        }
+    }
+}
