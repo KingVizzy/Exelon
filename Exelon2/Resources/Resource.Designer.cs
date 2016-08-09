a71c70a9ede62c4907cae6d4dde13fbd3c645e7d
KingVizzy <vallery.salomon@gmail.com>
2016-08-09 13:25:31 -0400

    More changes

--- Exelon2/Resources/Resource.Designer.cs
+++ Exelon2/Resources/Resource.Designer.cs
@@ -112,26 +112,29 @@ namespace Exelon2
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f070005
+			public const int btnDialogEmail = 2131165189;
+			
 			// aapt resource value: 0x7f070002
-			public const int btnDialogEmail = 2131165186;
-			
-			// aapt resource value: 0x7f070006
-			public const int btnPayBill = 2131165190;
-			
-			// aapt resource value: 0x7f070009
-			public const int btnSignIn = 2131165193;
-			
-			// aapt resource value: 0x7f07000b
-			public const int btnSignUp = 2131165195;
+			public const int btnDialogSignIn = 2131165186;
 			
 			// aapt resource value: 0x7f070007
-			public const int btnViewUsage = 2131165191;
+			public const int btnPayBill = 2131165191;
+			
+			// aapt resource value: 0x7f07000a
+			public const int btnSignIn = 2131165194;
+			
+			// aapt resource value: 0x7f07000c
+			public const int btnSignUp = 2131165196;
+			
+			// aapt resource value: 0x7f070008
+			public const int btnViewUsage = 2131165192;
 			
 			// aapt resource value: 0x7f070000
 			public const int txtAccountID = 2131165184;
 			
-			// aapt resource value: 0x7f070008
-			public const int txtCreateAccount = 2131165192;
+			// aapt resource value: 0x7f070009
+			public const int txtCreateAccount = 2131165193;
 			
 			// aapt resource value: 0x7f070004
 			public const int txtEmail = 2131165188;
@@ -139,11 +142,11 @@ namespace Exelon2
 			// aapt resource value: 0x7f070003
 			public const int txtFirstName = 2131165187;
 			
-			// aapt resource value: 0x7f070005
-			public const int txtHomeScreen = 2131165189;
+			// aapt resource value: 0x7f070006
+			public const int txtHomeScreen = 2131165190;
 			
-			// aapt resource value: 0x7f07000a
-			public const int txtOr = 2131165194;
+			// aapt resource value: 0x7f07000b
+			public const int txtOr = 2131165195;
 			
 			// aapt resource value: 0x7f070001
 			public const int txtPassword = 2131165185;
